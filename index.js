1e03adde1bfdae8c13bfd439fa15922973b11908
Brigitte G <urbrigette@gmail.com>
2020-10-02 20:01:01 -0400

    title question

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+// setting my variables requiring my other files
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMd = require("./utils/generateMarkdown");
-
+// passing the questions for the user 
 inquirer.prompt([
     {
-       
-
-    }
+       type:"input",
+       message:"What is the title of your project?",
+       name:"Title"
+    },
 ])
+.then(answers => {
+    // Use user feedback for... whatever!!
+  })
+  .catch(error => {
+    if(error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else when wrong
+    }
+  });
